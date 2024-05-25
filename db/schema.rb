a53b1588d8519c706e8c22cd05909f10465dc118
kimitaWanjohi <kimitawanjohi7923@gmail.com>
2024-05-25 13:39:56 +0300

    Scaffold articles.

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_25_091155) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_25_102218) do
   create_table "articles", force: :cascade do |t|
-    t.string "tilte"
+    t.string "title"
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
